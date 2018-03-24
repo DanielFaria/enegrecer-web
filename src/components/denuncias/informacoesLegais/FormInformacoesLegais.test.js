9389091246c1c4ae3803a482f32a23f50d29340f
Daniel Faria <lenin@stack.com>
2018-03-24 15:33:07 -0300

    Form das informaçoes legais criado

---
+++ src/components/denuncias/informacoesLegais/FormInformacoesLegais.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import toJson from 'enzyme-to-json';
+import { mount } from 'enzyme';
+import InformacoesLegais from './FormInformacoesLegais';
+
+describe('DenuncianteForm', () => {
+  it('deve seguir a estrutura definida ', () => {
+    const handleChange = jest.fn();
+    const informacoesLegais = mount(<InformacoesLegais />);
+    const informacoesLegaisJson = toJson(informacoesLegais);
+    expect(informacoesLegaisJson).toMatchSnapshot();
+  });
+});
